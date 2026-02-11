8b2b9885c045c2cf6788659cd3b3db95a78b78df
Jörgen Storvist <jorgen.storvist@techship.com>
2026-02-11 10:33:34 +0200

    Test for fixing deprecated pkg_resources import

--- src/infi/__init__.py
+++ src/infi/__init__.py
@@ -1,1 +1,12 @@
-__import__("pkg_resources").declare_namespace(__name__)
+#__import__("pkg_resources").declare_namespace(__name__)
+try:
+    import setuptools
+    version = setuptools.__version__.split('.')
+    if int(version[0]) <= 67 and int(version[1]) < 3:
+        try:
+            __import__('pkg_resources').declare_namespace(__name__)
+        except:
+            import pkgutil
+            __path__ = pkgutil.extend_path(__path__, __name__)
+except ImportError:
+    pass
